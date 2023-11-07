48589f8d3bf84d29292a7972216dc74756804ebb
Jhon Rios <r.jhonf@gmail.com>
2023-11-07 17:06:11 -0500

    use create, update and delete

--- services/user.service.js
+++ services/user.service.js
@@ -7,12 +7,14 @@ const { models } = require('../libs/sequelize')
 
 class UserService {
   constructor() {
-    this.pool = pool
-    this.pool.on('error', (err) => console.error(err))
+    // this.pool = pool
+    // this.pool.on('error', (err) => console.error(err))
   }
 
   async create(data) {
-    return data;
+    // Usamos el ORM para crear y le pasamos la data
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
@@ -29,17 +31,32 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+
+    // Validamos si el usuario existe o no
+    if (!user) {
+      // Si encuentra un error, la capa de routing va a detectarlo y lo envia a los middlwares de tipo error y se hace el procesamiento de dicho error (envía a user.router.js).
+      throw boom.notFound('user not found'); // Error de tipo boom
+    }
+    return user;
+    // return { id };
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    // Necesitamos ir, obtener un id y aplicar los cambios
+    const user = await this.findOne(id); // Usamos el método findOne para reusar codigo y obtener desde allí el error.
+    const rta = await user.update(changes);
+    return rta;
+    // return {
+    //   id,
+    //   changes,
+    // };
   }
 
   async delete(id) {
+    // Buscamos el id, y si existe, hacemos un await con el usuario y aplicamos destroy
+    const user = await this.findOne(id);
+    await user.destroy(); // Hacemos la eliminacion
     return { id };
   }
 }
